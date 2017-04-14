d0077ad5fc21d8c924069846c6524af5b2ec857f
Scott Smith <scottnelsonsmith@gmail.com>
2017-04-14 11:34:51 -0700

    Have something started

---
+++ JsonSurvivalCourse.elm
@@ -0,0 +1,96 @@
+
+import Html exposing (..)
+import Json.Decode exposing (..)
+
+
+main =
+    Html.beginnerProgram
+        { model = init
+        , view = view
+        , update = update
+        }
+
+
+-- MODEL
+
+
+type alias GithubUser =
+    { id : Int
+    , login : String
+    , name : String
+    , gravatarId : Maybe String
+    }
+
+
+type alias Model =
+    { github : String
+    }
+
+
+init : Model
+init =
+    {
+        github =
+            """
+            {
+              "login": "octocat",
+              "id": 583231,
+              "avatar_url": "https://avatars.githubusercontent.com/u/583231?v=3",
+              "gravatar_id": "",
+              "url": "https://api.github.com/users/octocat",
+              "html_url": "https://github.com/octocat",
+              "followers_url": "https://api.github.com/users/octocat/followers",
+              "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+              "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+              "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+              "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+              "organizations_url": "https://api.github.com/users/octocat/orgs",
+              "repos_url": "https://api.github.com/users/octocat/repos",
+              "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+              "received_events_url": "https://api.github.com/users/octocat/received_events",
+              "type": "User",
+              "site_admin": false,
+              "name": "The Octocat",
+              "company": "GitHub",
+              "blog": "http://www.github.com/blog",
+              "location": "San Francisco",
+              "email": "octocat@github.com",
+              "hireable": null,
+              "bio": null,
+              "public_repos": 7,
+              "public_gists": 8,
+              "followers": 1667,
+              "following": 6,
+              "created_at": "2011-01-25T18:44:36Z",
+              "updated_at": "2016-12-23T05:44:24Z"
+            }
+            """
+    }
+
+
+-- UPDATE
+
+
+type Msg
+    = Reset
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Reset ->
+      model
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ pre []
+              [ text model.github
+              ]
+        , pre []
+              [ text (toString (decodeString bool "true"))]
+        ]
